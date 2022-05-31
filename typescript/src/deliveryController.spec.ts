174c1570a067558cbe44e8b7f07e628bb9408071
Andrew Fraser <a.vector@gmail.com>
2022-05-31 17:44:58 +0200

    add requestFeedback to IFeedbackRequester

--- typescript/src/deliveryController.spec.ts
+++ typescript/src/deliveryController.spec.ts
@@ -5,6 +5,12 @@ const GREATER_THAN_TEN_MINUTES = 1000 * 60 * 10 + 1;
 
 class FakeFeedbackRequester implements IFeedbackRequester {
     public sendCalls : {address:string, subject:string, message:string}[] = [];
+    public requestedFeedbackCalls: {delivery: Delivery, message:string}[] = [];
+
+    requestFeedback(delivery: Delivery, message: string): Promise<any> {
+        this.requestedFeedbackCalls.push({delivery, message});
+        return Promise.resolve(undefined);
+    }
 
     send(address: string, subject: string, message: string): Promise<any> {
         this.sendCalls.push({address, subject, message});
@@ -32,57 +38,64 @@ function expectDeliveryToBe(delivery: Delivery, expectedArrived: boolean, expect
 }
 
 describe("WTf does the controller do", () => {
-    it("should call the feedbackRequester when a delivery has been made", ()=> {
+    it("should call the feedbackRequester when a delivery has been made", async () => {
         const id = "123";
-        const deliveries : Delivery[] = [createDelivery(id)];
+        const deliveries: Delivery[] = [createDelivery(id)];
         const fakeFeedbackRequester = new FakeFeedbackRequester();
-        const deliveryController  = new DeliveryController(deliveries, fakeFeedbackRequester);
-        const deliveryEvent = {id: id,
+        const deliveryController = new DeliveryController(deliveries, fakeFeedbackRequester);
+        const deliveryEvent = {
+            id: id,
             timeOfDelivery: new Date(),
-            location: {longitude: 123, latitude: 432}};
-        deliveryController.updateDelivery(deliveryEvent);
+            location: {longitude: 123, latitude: 432}
+        };
+        await deliveryController.updateDelivery(deliveryEvent);
         let expectedMessage = `Regarding your delivery today at ${deliveries[0].timeOfDelivery}. How likely would you be to recommend this delivery service to a friend? Click <a href='url'>here</a>`
 
-        expect(fakeFeedbackRequester.sendCalls).toHaveLength(1);
-        expect(fakeFeedbackRequester.sendCalls[0].address).toBe(deliveries[0].contactEmail);
-        expect(fakeFeedbackRequester.sendCalls[0].subject).toBe("Your feedback is important to us");
-        expect(fakeFeedbackRequester.sendCalls[0].message).toBe(expectedMessage);
+        expect(fakeFeedbackRequester.requestedFeedbackCalls).toHaveLength(1);
+        expect(fakeFeedbackRequester.requestedFeedbackCalls[0].delivery).toBe(deliveries[0]);
+        expect(fakeFeedbackRequester.requestedFeedbackCalls[0].message).toBe(expectedMessage);
     })
 
-    it("updates the delivery when it is made", () => {
+    it("updates the delivery when it is made", async () => {
         const id = "123";
-        const deliveries : Delivery[] = [createDelivery(id)];
+        const deliveries: Delivery[] = [createDelivery(id)];
         const fakeGateway = new FakeFeedbackRequester();
-        const deliveryController  = new DeliveryController(deliveries, fakeGateway);
-        const deliveryEvent = {id: id,
+        const deliveryController = new DeliveryController(deliveries, fakeGateway);
+        const deliveryEvent = {
+            id: id,
             timeOfDelivery: deliveries[0].timeOfDelivery,
-            location: {longitude: 123, latitude: 432}};
-        deliveryController.updateDelivery(deliveryEvent);
+            location: {longitude: 123, latitude: 432}
+        };
+        await deliveryController.updateDelivery(deliveryEvent);
 
         expectDeliveryToBe(deliveries[0], true, true, deliveryEvent.timeOfDelivery);
     })
 
     describe("when there are two deliveries", () => {
-        it("should sends an email to each delivery when it is made", () => {
+        it("should sends an email to each delivery when it is made", async () => {
             const id = "1";
             const id2 = "2";
-            const deliveries : Delivery[] = [createDelivery(id), createDelivery(id2)];
+            const deliveries: Delivery[] = [createDelivery(id), createDelivery(id2)];
             const fakeFeedbackRequester = new FakeFeedbackRequester();
-            const deliveryController  = new DeliveryController(deliveries, fakeFeedbackRequester);
-            const deliveryEvent1 = {id: id,
+            const deliveryController = new DeliveryController(deliveries, fakeFeedbackRequester);
+            const deliveryEvent1 = {
+                id: id,
                 timeOfDelivery: deliveries[0].timeOfDelivery,
-                location: {longitude: 123, latitude: 432}};
-            deliveryController.updateDelivery(deliveryEvent1);
+                location: {longitude: 123, latitude: 432}
+            };
+            await deliveryController.updateDelivery(deliveryEvent1);
 
             expectDeliveryToBe(deliveries[0], true, true, deliveryEvent1.timeOfDelivery);
             expectDeliveryToBe(deliveries[1], false, false, deliveries[1].timeOfDelivery);
 
-            expect(fakeFeedbackRequester.sendCalls).toHaveLength(2);
-            expect(fakeFeedbackRequester.sendCalls[1].subject).toBe("Your delivery will arrive soon.");
+            expect(fakeFeedbackRequester.sendCalls).toHaveLength(1);
+            expect(fakeFeedbackRequester.sendCalls[0].subject).toBe("Your delivery will arrive soon.");
 
-            const deliveryEvent2 = {id: id2,
+            const deliveryEvent2 = {
+                id: id2,
                 timeOfDelivery: deliveries[1].timeOfDelivery,
-                location: {longitude: 123, latitude: 432}};
+                location: {longitude: 123, latitude: 432}
+            };
             deliveryController.updateDelivery(deliveryEvent2);
 
             expectDeliveryToBe(deliveries[0], true, true, deliveryEvent1.timeOfDelivery);
@@ -90,22 +103,26 @@ describe("WTf does the controller do", () => {
         })
 
         describe("and first one is late", () => {
-            it("should update the eta for the second one", () => {
+            it("should update the eta for the second one", async () => {
                 const id = "1";
                 const id2 = "2";
-                const deliveries : Delivery[] = [createDelivery(id), createDelivery(id2)];
+                const deliveries: Delivery[] = [createDelivery(id), createDelivery(id2)];
                 const fakeFeedbackRequester = new FakeFeedbackRequester();
-                const deliveryController  = new DeliveryController(deliveries, fakeFeedbackRequester);
-                const deliveryEvent1 = {id: id,
+                const deliveryController = new DeliveryController(deliveries, fakeFeedbackRequester);
+                const deliveryEvent1 = {
+                    id: id,
                     timeOfDelivery: new Date(deliveries[0].timeOfDelivery.getTime() + GREATER_THAN_TEN_MINUTES),
-                    location: {longitude: 123, latitude: 432}};
-                deliveryController.updateDelivery(deliveryEvent1);
+                    location: {longitude: 123, latitude: 432}
+                };
+                await deliveryController.updateDelivery(deliveryEvent1);
 
                 expectDeliveryToBe(deliveries[0], true, false, deliveryEvent1.timeOfDelivery);
 
-                const deliveryEvent2 = {id: id2,
+                const deliveryEvent2 = {
+                    id: id2,
                     timeOfDelivery: new Date(deliveryEvent1.timeOfDelivery.getTime() + GREATER_THAN_TEN_MINUTES),
-                    location: {longitude: 123, latitude: 432}};
+                    location: {longitude: 123, latitude: 432}
+                };
                 deliveryController.updateDelivery(deliveryEvent2);
 
                 expectDeliveryToBe(deliveries[1], true, false, deliveryEvent2.timeOfDelivery);
--- typescript/src/emailGateway.ts
+++ typescript/src/emailGateway.ts
@@ -1,14 +1,14 @@
 import nodemailer from "nodemailer";
 import {Delivery} from "./deliveryController";
 
-
 export interface IFeedbackRequester {
     send(address: string, subject: string, message: string): Promise<any>;
+    requestFeedback(delivery: Delivery, message: string): Promise<any>;
 }
 
 export class EmailGateway implements IFeedbackRequester {
-
     #transport: nodemailer.Transporter
+    feedbackSubject = "Your feedback is important to us";
 
     constructor() {
         this.#transport = nodemailer.createTransport({
@@ -17,6 +17,15 @@ export class EmailGateway implements IFeedbackRequester {
                 secure: false,
                 logger: true
         } );
+
+    }
+
+    async requestFeedback(delivery: Delivery, message: string) {
+        return this.#transport.sendMail({
+            subject: this.feedbackSubject,
+            to: delivery.contactEmail,
+            text: message
+        })
 
     }
 
