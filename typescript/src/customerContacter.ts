b07280abad8ab2d845b6bd040b656bf906bd2bbc
Andrew Fraser <a.vector@gmail.com>
2022-06-06 15:17:59 +0200

    make class fields const as they're only used in single methods each

--- typescript/src/customerContacter.ts
+++ typescript/src/customerContacter.ts
@@ -11,15 +11,17 @@ export class CustomerContacter implements ICustomerContacter {
     constructor(private emailGateway: IEmailGateway, private smsGateway: ISmsGateway) {}
 
     async requestFeedback(delivery: Delivery, message: string): Promise<any> {
+        const feedbackSubject = "Your feedback is important to us";
         if (delivery.preferredContactMethod === ContactMethod.Email)
-            return this.emailGateway.sendEmail(this.#feedbackSubject, delivery.contactEmail, message);
+            return this.emailGateway.sendEmail(feedbackSubject, delivery.contactEmail, message);
         else if (delivery.preferredContactMethod === ContactMethod.Sms)
             return this.smsGateway.sendSms(delivery.mobileNumber, message);
     }
 
     async notifyDeliveryEta(delivery: Delivery, message: string): Promise<any> {
+        const notifyDeliverySubject = "Your delivery will arrive soon.";
         if (delivery.preferredContactMethod === ContactMethod.Email)
-            return this.emailGateway.sendEmail(this.#notifyDeliverySubject, delivery.contactEmail, message);
+            return this.emailGateway.sendEmail(notifyDeliverySubject, delivery.contactEmail, message);
         else if (delivery.preferredContactMethod === ContactMethod.Sms)
             return this.smsGateway.sendSms(delivery.mobileNumber, message);
     }
