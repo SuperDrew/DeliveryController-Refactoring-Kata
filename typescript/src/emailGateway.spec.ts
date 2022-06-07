e400bcc9ae8309e894e960ac8b0068749a9b5d74
Andrew Fraser <a.vector@gmail.com>
2022-06-07 14:45:00 +0200

    added arrange, act, assert formatting  - I dunno maybe it's cool maybe it's not, if it's needed does that mean the code isn't readable enough without it?

--- typescript/src/emailGateway.spec.ts
+++ typescript/src/emailGateway.spec.ts
@@ -7,8 +7,13 @@ describe("When sending an email", () => {
     })
 
     it('should use the correct nodemailer config', () => {
+        // Arrange
         const createTransportSpy = jest.spyOn(nodemailer, 'createTransport');
+
+        // Act
         new EmailGateway();
+
+        // Assert
         expect(createTransportSpy).toHaveBeenCalled();
         expect(createTransportSpy.mock.calls[0][0]).toStrictEqual({
             host: 'localhost',
@@ -19,12 +24,16 @@ describe("When sending an email", () => {
     })
 
     it('Should use the correct, subject, address and message', async () => {
+        // Arrange
         const createTransportSpy = jest.spyOn(nodemailer, 'createTransport');
         const emailGateway = new EmailGateway();
         const transporter = createTransportSpy.mock.results[0].value;
         const sendMailSpy = jest.spyOn(transporter, "sendMail").mockImplementation(() => Promise.resolve());
 
+        // Act
         await emailGateway.sendEmail("subject", "to", "text");
+
+        // Assert
         expect(sendMailSpy).toHaveBeenCalled();
         expect(sendMailSpy.mock.calls[0][0]).toStrictEqual({
             subject: "subject",
